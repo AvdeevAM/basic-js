3fde70930e00586b85a020ccf496275601b78934
avdeevam <avdeev.am471@gmail.com>
2021-04-09 22:54:55 +0300

    feat: add solutions to tasks 5,9, fix solutions 2,3

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if ((!sampleActivity) || (typeof sampleActivity != 'string') || (isNaN(sampleActivity)) || (Number(sampleActivity) < 0) || !(isFinite(sampleActivity))){
+  if (!(isFinite(sampleActivity)) || (!sampleActivity) || (typeof sampleActivity != 'string') || (isNaN(sampleActivity)) || (Number(sampleActivity) < 0)){
     return false;
   }
   return Math.ceil((Math.log(MODERN_ACTIVITY / Number(sampleActivity))) / (0.693 / HALF_LIFE_PERIOD));
